3be9ba3177dc1953327afe7de807403dd460ad2a
Al <87392984+LiShanDa2021@users.noreply.github.com>
2022-10-01 13:47:01 -0500

    added radio buttons

--- ff_logic.js
+++ ff_logic.js
@@ -14,17 +14,15 @@ const candidates =
         Amphibian: ["Tree Frog", "Cane Toad", "Salamander", "Axolotl", "Newt", "Caecilian", "Bullfrog"]
     },
     {
-        Fish: ["Tuna", "Marlin", "Sturgeon", "Guppy", "Mackerel", "Piranha", "Shark"]
+        Cryptid: ["Sasquatch", "Chupacabra", "Loch Ness Monster", "Yeti", "Jackalope", "Hodag", "Mothman"]
     }
 ]
 
-
-
 var ballotCand = []
 var ballotParty = []
-ballotStack = {}
-
-
+var ballotStack = {}
+var noBallots = 25
+var ballotCounter = 0
 
 function createBallot(candidates)
 {
@@ -35,53 +33,44 @@ function createBallot(candidates)
     {
         party = Object.keys(avail_candidates[i])[0];
 
-        candidate = (Object.values(avail_candidates[i])[0][(Math.floor(Math.random() * (avail_candidates.length)))]);
+        candidate = (Object.values(avail_candidates[i])[0][(Math.floor(Math.random() * (avail_candidates[i][party].length)))]);
 
         
         //delete chosen candidate from avail_candidates once we have multiple candidates same party
 
-        console.log(candidate + " - " + party);
+        //console.log(candidate + " - " + party);
 
         ballot.push({candidate : candidate, party : party, first: 0, second: 0, third: 0, fourth: 0, fifth: 0})
     }
-    console.log(ballot)
+    //console.log(ballot)
     return ballot
 }
 
+function createBallotVotes(ballot) {
 
-
-function createBallotStack(ballot) {
-    console.log("This function doesn't do anything yet. . . . . .or does it?")
-    
-    ballotStack = []
-    noBallots = 3
-
-    for (i = 0; i < noBallots; i++) 
-    {
-        possibilities = 
+    possibilities = 
     [
-        [1,0,0,0,0],
-        [0,1,0,0,0],
-        [0,0,1,0,0],
-        [0,0,0,1,0],
-        [0,0,0,0,1]
+        ["x",0,0,0,0],
+        [0,"x",0,0,0],
+        [0,0,"x",0,0],
+        [0,0,0,"x",0],
+        [0,0,0,0,"x"]
     ];
-        ballotVotes = shuffle(possibilities)
-        console.log(ballotVotes)
+   
+    ballotVotes = possibilities.sort(() => Math.random() - 0.5)
+        //console.log(ballotVotes)
         //console.log(ballot)
-        for (j = 0; j < 5; j++) {
-            ballot[j].first = possibilities[j][0];
-            ballot[j].second = possibilities[j][1];
-            ballot[j].third = possibilities[j][2];
-            ballot[j].fourth = possibilities[j][3];
-            ballot[j].fifth = possibilities[j][4];
+    for (j = 0; j < 5; j++) {
+        ballot[j].first = ballotVotes[j][0];
+        ballot[j].second = ballotVotes[j][1];
+        ballot[j].third = ballotVotes[j][2];
+        ballot[j].fourth = ballotVotes[j][3];
+        ballot[j].fifth = ballotVotes[j][4];
         }
-        console.log(ballot);
-    }
+    
     //console.log(ballotStack)
-    return ballotStack
+    return ballot
 };
-
 
 function shuffle(array) {
     let currentIndex = array.length,  randomIndex;
@@ -101,10 +90,8 @@ function shuffle(array) {
     return array;
   }
 
-
-
 function createTable(ballot) {
-    var tbody = d3.select("tbody");
+    var tbody = d3.select("#ballot");
     tbody.html("");
     ballot.forEach((cand) => 
     {
@@ -117,14 +104,104 @@ function createTable(ballot) {
     });
 }
 
-createBallot(candidates)
-createBallotStack(ballot)
-createTable(ballot)
 
-// [
-//     {first: 1, second: 0, third: 0, fourth: 0, fifth: 0},
-//     {first: 0, second: 1, third: 0, fourth: 0, fifth: 0},
-//     {first: 0, second: 0, third: 1, fourth: 0, fifth: 0},
-//     {first: 0, second: 0, third: 0, fourth: 1, fifth: 0},
-//     {first: 0, second: 0, third: 0, fourth: 0, fifth: 1}
-// ];
+function createScorecard(ballot)
+{
+    var scoreBody = d3.select("#scorecard");
+    scoreBody.html("");
+    remain_cand = 5
+    let row = scoreBody.append("tr");
+
+    //create radio buttons
+    for (i = 0; i < remain_cand; i++) 
+    {
+        let cell = row.append("td");
+        cell.html("<input type="+"radio"+" name="+"vote-getter"+" value="+i+">")
+        //console.log(candidate_data['candidate'])
+    }
+
+    let row2 = scoreBody.append("tr");
+
+    //create choice labels
+    for (i = 0; i < remain_cand; i++) 
+    {
+        let cell = row2.append("td");
+        candidate_data=ballot[i]
+        cellText = (candidate_data['candidate']);
+        cell.text(cellText);
+    }
+
+    let row3 = scoreBody.append("tr");
+
+    //create candidate votes
+    for (i = 0; i < remain_cand; i++) {
+        let cell = row3.append("td")
+        cell.text("Votes:")
+    }
+    
+    let row4 = scoreBody.append("tr");
+    //create votes needed to win
+    for (i = 0; i < remain_cand; i++) {
+        let cell = row4.append("td")
+        cell.text("Needed to win")
+    }
+    
+};
+
+function scoreBallot() {
+    let changedElement = d3.select(this);
+    let elementValue = changedElement.property("value");
+    console.log(ballot[elementValue]['candidate']);
+
+    ballotCounter++
+    createBallotVotes(ballot)
+    createTable(ballot)
+    createScorecard(ballot)
+    d3.selectAll("input").on("click", scoreBallot);
+}
+
+createBallot(candidates)
+createBallotVotes(ballot)
+createTable(ballot)
+createScorecard(ballot)
+
+d3.selectAll("input").on("click", scoreBallot);
+
+
+// function createBallotStack(ballot) {
+    
+//     ballotStack = []
+//     possibilities = 
+//     [
+//         ["x",0,0,0,0],
+//         [0,"x",0,0,0],
+//         [0,0,"x",0,0],
+//         [0,0,0,"x",0],
+//         [0,0,0,0,"x"]
+//     ];
+//     for (i = 0; i < noBallots; i++) 
+//     {
+//         ballotVotes = possibilities.sort(() => Math.random() - 0.5)
+//         //console.log(ballotVotes)
+//         //console.log(ballot)
+//         for (j = 0; j < 5; j++) {
+//             ballot[j].first = ballotVotes[j][0];
+//             ballot[j].second = ballotVotes[j][1];
+//             ballot[j].third = ballotVotes[j][2];
+//             ballot[j].fourth = ballotVotes[j][3];
+//             ballot[j].fifth = ballotVotes[j][4];
+//         }
+//         ballotStack.push(ballot)
+//     }
+//     //console.log(ballotStack)
+//     return ballotStack
+// };
+
+// function runElection(ballotStack)
+// {
+//     for (i = 0; i < noBallots; i++) 
+//     {
+//         createTable(ballotStack[i])
+//         createScorecard(ballotStack[i])
+//     }
+// }
